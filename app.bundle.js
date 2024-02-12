06b074e7a94777d66bab63ef9c11cf29a02c6a65
Nurudeen Abdul-Majeede <abdulmajeednurudeen48@gmail.com>
2024-02-12 11:56:11 -0800

    Added test dependencies and createed the test file and configs

--- app.bundle.js
+++ app.bundle.js
@@ -1,2 +1,2 @@
-console.log("Battleship");
+(()=>{"use strict";const t=new class{constructor(){this.trial="hello battleship trial"}trialMethod(){console.log("battleship trial method")}};console.log(t.trial),t.trialMethod()})();
 //# sourceMappingURL=app.bundle.js.map
