e21ebc82cc0c9122817169972155399ed151ba52
Nurudeen Abdul-Majeede <abdulmajeednurudeen48@gmail.com>
2024-02-13 11:18:52 -0800

    Added the Ship implementation code to make it pass

--- src/js-files/app-logic.js
+++ src/js-files/app-logic.js
@@ -1,3 +1,21 @@
-class Ship {}
+class Ship {
+  constructor(length, hitTimes) {
+    this.length = length;
+    this.hitTimes = hitTimes;
+  }
+
+  hit() {
+    if (!this.isSunk()) {
+      this.hitTimes++;
+    }
+  }
+
+  isSunk() {
+    if (this.hitTimes === this.length) {
+      return true;
+    }
+    return false;
+  }
+}
 
 export default Ship;
