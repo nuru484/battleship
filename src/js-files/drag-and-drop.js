90a19bb34dc94e870e2ea9d478afe4198e01fdb2
Nurudeen Abdul-Majeede <abdulmajeednurudeen48@gmail.com>
2024-02-18 15:56:35 -0800

    Added comments to the drag and drop implementation for easy readability

--- src/js-files/drag-and-drop.js
+++ src/js-files/drag-and-drop.js
@@ -1,30 +1,40 @@
 const ships = document.querySelectorAll('.ship');
 const playerCells = document.querySelectorAll('.playerCells');
 
+// Events on dragged elements
+
+// Drag start helper function
 const dragStart = (e) => {
   setTimeout(() => {
     e.target.classList.add('hide');
   }, 0);
 };
 
+// Drag Start
 ships.forEach((ship) => {
   ship.addEventListener('dragstart', dragStart);
 });
 
+// Events on drop target
+
+// Drag enter helper function
 const dragEnter = (e) => {
   e.preventDefault();
   e.target.classList.add('drag-over');
 };
 
+// Drag over helper function
 const dragOver = (e) => {
   e.preventDefault();
   e.target.classList.add('drag-over');
 };
 
+// Drag leave helper function
 const dragLeave = (e) => {
   e.target.classList.remove('drag-over');
 };
 
+// Drop helper function
 const drop = (e) => {
   e.target.classList.remove('drag-over');
   const dragable = document.querySelector('.hide');
@@ -32,6 +42,7 @@ const drop = (e) => {
   dragable.classList.remove('hide');
 };
 
+// Drop target events on the cells
 playerCells.forEach((cell) => {
   cell.addEventListener('dragenter', dragEnter);
   cell.addEventListener('dragover', dragOver);
